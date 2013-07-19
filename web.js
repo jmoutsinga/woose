26c4f8370d85146721b303c37d2d616752e323ba
Julien Moutsinga <julien.moutsinga@gmail.com>
2013-07-19 04:23:39 +0200

    Startup Engineering -> HW3 -> part1 -> step 6,7,8 : sending the content of index.html

--- web.js
+++ web.js
@@ -1,12 +1,20 @@
+
+
 var express = require('express');
+var fs = require('fs');
+
+// instantiate the application
 var app = express();
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2 !');
+    var htmlContent = fs.readFileSync('./index.html', {encoding : 'utf8'});
+    response.send(htmlContent);
 });
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
